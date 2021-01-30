c3d99d6d20e1cfc46ea650a62b569c9c1b4521d9
Victoria Thorsen <victoriathorsenn@gmail.com>
2021-01-29 21:44:07 -0500

    commit tree f6ea77d0a1e5a4198523a24b5b1bf85a98112d5a

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
+
 class App < Sinatra::Base
+    get '/' do
+        erb :user_input
+    end
+
+    post '/piglatinize' do
+       sentence = PigLatinizer.new
+       @piglatin = sentence.piglatinize(params[:user_phrase])
+       erb :results
+    end
 end
